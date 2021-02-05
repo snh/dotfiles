10d0e5ae1ac7002f17cc54bd3a2153669927fe78
Steven Honson <steven@honson.id.au>
2021-02-06 00:55:13 +1100

    Updates

--- aliases.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-alias jq='docker run -it --rm jess/jq jq'
---
+++ vscode/extensions.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+code --install-extension GitHub.codespaces
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension ms-vscode-remote.remote-ssh-edit
