8961149c91e898da79152fb9d55b9b33f7ed7b2a
Steven Honson <snh@github.com>
2021-02-12 18:03:32 +1100

    Update extensions.sh

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 code --install-extension GitHub.codespaces
+code --install-extension GitHub.vscode-pull-request-github
 code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-vscode-remote.remote-containers
 code --install-extension ms-vscode-remote.remote-ssh
 code --install-extension ms-vscode-remote.remote-ssh-edit
+code --install-extension redhat.vscode-yaml
